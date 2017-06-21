eb61fc8e4ce68dba40722edf846cfc4dedc1ce80
周文耿 <412582902@qq.com>
2017-06-21 10:08:54 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  devise_for :user
   namespace :admin do
     resources :products
   end
-  devise_for :users
-  root 'welcome#index'
+
+  resources :products
 end
